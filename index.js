67aad6ae2a31e87b05f3461d925e18aab39854ef
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-12-14 14:42:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import Image from 'components/Image';
 
+class ModuleDemo extends Component {
 
   render(){
 	  
     return (
       <div className='FranklyModulesDemo'>
 	<h2 className='demo_text'>{this.props.text}</h2>
-	<p>This text is not editable!!</p>
+	<p>This text is not editable!</p>
 	<Image width={280} src={'http://franklydemo.images.worldnow.com/images/15208935_G.jpeg'} alt={'alt'} />
       </div>
     );
