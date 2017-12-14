f856e58aea4fe9840217d8ab5d64d90f6cb611bf
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-12-14 16:11:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class ModuleDemo extends Component {
 	<p>This text is not editable!</p>
 	<Image class="nadia" width={280} src={'http://tropicsgourmet.com/wp-content/uploads/2016/02/Ha_Long_Bay_Vietnam.jpg'} alt={'alt'} />
 	<Image class="muzna" width={280} src={'http://franklydemo.images.worldnow.com/images/15208935_G.jpeg'} alt={'alt'} />
+	<Image class="muzna" width={280} src={'http://wnow.images.worldnow.com/images/5863946_G.gif} alt={'alt'} />    
       </div>
     );
 	  
